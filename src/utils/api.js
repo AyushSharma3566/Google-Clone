0d1b8cd8d494da5abe8937740c65144886c1a454
Ayush Sharma <78665391+AyushSharma3566@users.noreply.github.com>
2024-06-09 01:22:28 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 const params = {
-  key: 'AIzaSyAxH0PS_iHpMigT8iTwJuuqB3wpDtE6ATQ',
-  cx: '5227b2d6e17204c71'
+  key: '',
+  cx: ''
 }
 export const fetchDataFromApi = async (payLoad) => {
     const { data } = await axios.get(BASE_URL, {
